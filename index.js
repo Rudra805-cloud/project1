cbe779572b0293395df0a6d4483ff2ba0ee11228
Rudra805 <rudramishra9949@gmail.com>
2026-02-06 23:54:58 +0530

    Include  Post patch and delete operation

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from "express"
 import users from "./MOCK_DATA.json" with { type: "json" }
+import fs from "fs"
+import { error } from "console";
 const app=express();
 const PORT=8000;
+
+//middleware to load data from req and create a object and put in body
+app.use(express.urlencoded({extended :false}))
 //routes
 app.get("/api/users",(req,res)=>{
     return res.json(users);
@@ -12,31 +17,12 @@ app.get("/users",(req,res)=>{
         ${users.map((user)=>`<li>${user.first_name} </li>`).join("")}
     </ul>
     `
-    res.send(html);
+    res.send(html)
 })
-/* app.get("/api/users/:id",(req,res)=>{
-    const id=Number(req.params.id)//paramas=>url ke andar parameter me variable hai usko access karne ke liye
-    //const user=users.find((user)=>user.id===id)    //we dont have to return inside function;
-    //return res.send(user);
-})
-app.post("/api/users/:id",(req,res)=>{
-    //add user
-    return res.send("status:pending")
-})
-app.patch("/api/users/:id",(req,res)=>{
-    //edit user
-    return res.send("status:pending")
-})
-app.delete("/api/users/:id",(req,res)=>{
-    //delete user
-    return res.send("status:pending")
-})
-*/
-// When we have to do multiple task on same route we do in suchh way 
+// {/ When "we have"}to do multiple task on same route we do in suchh way 
 
-app
-.route("/api/users/:id")
-.get((req,res)=>{
+
+app.get("/api/users/:id",(req,res)=>{
      const id=Number(req.params.id)
     const user=users.find((user)=>user.id==id);
     if(!user){
@@ -44,36 +30,57 @@ app
     }
    return res.send(user)
 })
-.post((req,res)=>{
-    //add user
-    return res.send("status:pending")
+app.post("/api/users",(req,res)=>{
+    const body=req.body;
+    const newUser={
+      ...body,
+      id:users.length+1
+    }
+    users.push(newUser);
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users), (err,data)=>{
+                if (err) {
+                   return res.status(500).json({ status: "error" });
+                  }
+
+           return res.json({status:"user added",userid:`${users.length }`})
+    })
 })
 
-.patch((req,res)=>{
-    //edit user
-    return res.send("status:pending")
+app.patch("/api/users/:id",(req,res)=>{
+      const body=req.body;
+      const userid=Number(req.params.id)
+      const index=users.findIndex(user=>user.id==userid)
+       if(index==-1){
+       return res.status(404).json({ message: "User not found" });
+    }  
+     // Merge old user with new fields
+    users[index] = {
+        ...users[index],
+        ...body
+    };
+     fs.writeFile("./MOCK_DATA.json",JSON.stringify(users), (err)=>{
+                if (err) {
+                   return res.status(500).json({ status: "error" });
+                  }
+
+           return res.json({status:"user updated",userid:`${userid}`})
+    })
 })
-.delete((req,res)=>{
-    //delete user
-    return res.send("status:pending")
+app.delete("/api/users/:id",(req,res)=>{
+     const id=Number(req.params.id)
+    const index=users.findIndex(user=>user.id==id);
+    if(index==-1){
+       return res.status(404).json({ message: "User not found" });
+    }
+    users.splice(index,1);
+ 
+   fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(error)=>{
+     if (error) {
+       return res.status(500).json({ status: "error" });
+     }
+     return res.json({status:"user deleted",userid:`${id }`})
+   })
+   
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(PORT,()=>console.log(`Server started at port ${PORT}`))
